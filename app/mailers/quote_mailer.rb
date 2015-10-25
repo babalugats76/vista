926b40a2209736d993256eb9ff03ac379556cfe3
James Colestock <james@colestock.com>
2015-10-25 18:47:15 +0000

    Updated simple, responsive email template

--- app/mailers/quote_mailer.rb
+++ app/mailers/quote_mailer.rb
@@ -1,7 +1,9 @@
 class QuoteMailer < ApplicationMailer
    default :to => "jamescolestock@gmail.com"
+   default :from => "james@colestock.com"
+   default :reply_to => "noreply@colestock.com"
    def rfq_email(quote)
       @quote = quote
-      mail(from: @quote.email, subject: "Request for Quote from #{@quote.name.split.map(&:capitalize).join(' ')}")
+      mail(from: @quote.email, cc: @quote.email, subject: "Request for Quote from #{@quote.name.split.map(&:capitalize).join(' ')}")
    end
 end
